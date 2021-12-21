3b5eb30308230b493fc8093746d08bd181459801
Darby Benfield <thinxing@hotmail.com>
2021-12-21 13:32:21 -0700

    complete lab

--- python-functions.py
+++ python-functions.py
@@ -9,16 +9,11 @@ import math
 
 num = int(input("Enter a number: "))
 def sum_to(n):
-  if n <= 1:
-      return n
-  else:
-      return n + sum_to(n-1)
+    if n <= 1:
+        return n
+    else:
+        return n + sum_to(n-1)
 print("The sum equals: ", sum_to(num))
-
-
-
-
-
 
 # Challenge 2
 # Write a function named largest that takes a list of numbers as an argument and returns the largest number in that list.
@@ -29,23 +24,22 @@ print("The sum equals: ", sum_to(num))
 
 
 
+# num = int(input("Enter a list of 5 numbers"))
+# def largest(n):
+#     largest = 0
+#     for x in n:
+#         largest = x
+#     else:
+#         return largest
+    
+    # I cant make this work with a user inputting numbers... 
 
 
+list_of_num = [5, 7, 26, 59, 75, 201]
+def largest(list_of_num):
+    return max(list_of_num)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(largest(list_of_num))
 
 
 # Challenge 3
@@ -58,29 +52,12 @@ print("The sum equals: ", sum_to(num))
 # occurances('fleep floop', 'fe')  # returns 0
 
 
+def occurances (string1, string2):
+    count = string1.count(string2)
+    return count 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# test
+# print(occurances("Bumblee Bee Buzzing", "B"))
 
 
 
@@ -91,3 +68,14 @@ print("The sum equals: ", sum_to(num))
 # product(-1, 4) # returns -4
 # product(2, 5, 5) # returns 50
 # product(4, 0.5, 5) # returns 10.0
+
+
+def product (*args):
+    product = 1
+    for arg in args:
+        product *= arg
+    return product 
+
+#test
+# print (product(2, 7))
+# print (product(9, 9, 1))
